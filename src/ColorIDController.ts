9b4e4618e5d9f99baa559314a71dfbf49d2e0602
Patrick Oyarzun <patrickoyarzun@gmail.com>
2018-04-13 07:35:21 -0400

    Simplify exposed interface to component. Now react looks like normal react

--- src/ColorIDController.ts
+++ src/ColorIDController.ts
@@ -2,35 +2,33 @@ import {Container, inject, injectable} from 'inversify'
 import * as React from 'react'
 import {ColorId} from './colorId/color'
 
-const setStateSymbol = "<setState>"
-const stateSymbol = "<state>"
+const setStateSymbol = Symbol.for("setState")
+const stateSymbol = Symbol.for("state")
 
-interface ControllerClass<T> {
-    initialState: T
-    new(): BaseController<T>
+interface ControllerClass<Props, State> {
+    initialState: State
+    new(): BaseController<Props, State>
 }
 
-export function bound<State, T extends BaseController<State>>(BoundComponent: React.ComponentClass<{controller: T}>, serviceIdentifier: ControllerClass<State>) {
-    return class extends React.Component<{container: Container}, {controllerState: State}> {
-        constructor(props: {container: Container}) {
+// Higher order component which makes inversify compatible with react.
+export function bound<Props, State, T extends BaseController<Props, State>>(BoundComponent: React.ComponentClass<Props>, serviceIdentifier: ControllerClass<Props, State>) {
+    return class extends React.Component<{ container: Container }, State> {
+        constructor(props: { container: Container }) {
             super(props)
 
-            this.state = {
-                controllerState: serviceIdentifier.initialState,
-            }
+            this.state = serviceIdentifier.initialState
         }
 
         render() {
+            // locally bind state / setState so they can be property-injected in BaseController
             const localContainer = new Container()
             localContainer.parent = this.props.container
-            localContainer.bind<(state: State) => void>(setStateSymbol).toConstantValue(s => {
-                return new Promise(resolve => {
-                    this.setState({controllerState: s}, resolve)
-                })
-            })
-            localContainer.bind<State>(stateSymbol).toDynamicValue(() => this.state.controllerState)
+            localContainer.bind(setStateSymbol).toConstantValue(this.setState.bind(this))
+            localContainer.bind(stateSymbol).toDynamicValue(() => this.state)
 
-            return React.createElement(BoundComponent, {controller: localContainer.get(serviceIdentifier)})
+            // auto wire a controller for the current render pass
+            let controller = localContainer.get(serviceIdentifier)
+            return React.createElement(BoundComponent, controller.props())
         }
     }
 }
@@ -41,37 +39,48 @@ export interface ColorIDState {
     loading: boolean
 }
 
-@injectable()
-class BaseController<T> {
-    @inject(setStateSymbol)
-    protected setState: (state: Partial<T>) => Promise<void>
-    @inject(stateSymbol)
-    protected state: Readonly<T>
+export interface ColorIDControllerProps {
+    color: string
+    colorName: string
+    loading: boolean
+    setColor(value: string): void
 }
 
+// The base controller which establishes some conventions to
+// *hopefully* make this architecture familiar to react developers.
+// with state / setState, you write code with all the same caveats as
+// when in react. (dont mutate state, setState is asynchronous, etc)
 @injectable()
-export class ColorIDController extends BaseController<ColorIDState> {
+abstract class BaseController<Props, State> {
+    @inject(setStateSymbol)
+    protected setState: (state: Partial<State>, callback?: () => void) => void
+    @inject(stateSymbol)
+    protected state: Readonly<State>
+
+    abstract props(): Props
+}
+
+// notice that this class is just a plain javascript class.
+// it can be constructed and inspected easily in tests.
+@injectable()
+export class ColorIDController extends BaseController<ColorIDControllerProps, ColorIDState> {
+    // the initial state for the component is owned by its controller
+    // this keeps the data all in one place
     static initialState: ColorIDState = {
         color: 'ff0000',
         loading: false,
-        colorName: 'Red'
+        colorName: 'Red',
     }
 
-    set color(value: string) {
-        console.log(value)
-        this.fetchColorName(value)
-    }
-
-    get color() {
-        return this.state.color
-    }
-
-    get loading() {
-        return this.state.loading
-    }
-
-    get colorName() {
-        return this.state.colorName
+    // adding this level of indirection for props() allows us to do any
+    // normal OOP code in the enclosing class, while retaining control of what
+    // gets exposed to the component
+    props() {
+        const {color, loading, colorName} = this.state
+        return {
+            setColor: (value: string) => this.fetchColorName(value),
+            color, loading, colorName,
+        }
     }
 
     private async fetchColorName(value: string) {
--- src/ColorIDView.tsx
+++ src/ColorIDView.tsx
@@ -1,24 +1,23 @@
 import * as React from 'react'
-import {bound, ColorIDController} from './ColorIDController'
+import {bound, ColorIDController, ColorIDControllerProps} from './ColorIDController'
 
-interface Props {
-    controller: ColorIDController
-}
-
-export class ColorIDView extends React.Component<Props> {
+// Notice the props here. They correspond exactly to our controller's props() method
+// this gives us a layer of indirection so we don't need to have a controller interface / fake / etc.
+// This component is a simple 'Presentational' component and easily testable.
+export class ColorIDView extends React.Component<ColorIDControllerProps> {
     handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.props.controller.color = event.target.value
+        this.props.setColor(event.target.value)
     }
 
     render() {
         return (
             <main>
                 <h1>Color ID</h1>
-                #<input key="colorInput" type="text" value={this.props.controller.color} onChange={this.handleColorChange}/>
+                #<input key="colorInput" type="text" value={this.props.color} onChange={this.handleColorChange}/>
 
-                <div style={{width: '50px', height: '50px', backgroundColor: `#${this.props.controller.color}`}}/>
+                <div style={{width: '50px', height: '50px', backgroundColor: `#${this.props.color}`}}/>
 
-                {this.props.controller.loading ? 'Loading...' : this.props.controller.colorName}
+                {this.props.loading ? 'Loading...' : this.props.colorName}
             </main>
         )
     }
